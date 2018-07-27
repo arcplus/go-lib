5261a17c9b119b1851393acaa019167e3f872194
elvizlai <sdrzlyz@gmail.com>
2018-07-27 14:26:19 +0800

    feat: add dup err check helper

--- mysql/mysql.go
+++ mysql/mysql.go
@@ -113,3 +113,21 @@ func Close() error {
 	}
 	return nil
 }
+
+// MySQLErr try conver mysql err to *mysql.MySQLError
+func MySQLErr(err error) *mysql.MySQLError {
+	if err == nil {
+		return nil
+	}
+	if e, ok := err.(*mysql.MySQLError); ok {
+		return e
+	}
+	return nil
+}
+
+// IsDupErr check if mysql error is ER_DUP_ENTRY
+// https://github.com/VividCortex/mysqlerr
+func IsDupErr(err error) bool {
+	e := MySQLErr(err)
+	return e != nil && e.Number == 1062
+}
