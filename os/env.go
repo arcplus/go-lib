59725f03312d6db2c2516d98045784b4c88f8931
elvizlai <sdrzlyz@gmail.com>
2018-08-14 15:11:18 +0800

    fix: default can empty

--- os/env.go
+++ os/env.go
@@ -5,9 +5,12 @@ import (
 )
 
 // Env read env with given key, if empty return defaultValue
-func Env(key string, defaultValue string) string {
+func Env(key string, defaultValue ...string) string {
 	if v := os.Getenv(key); v != "" {
 		return v
 	}
-	return defaultValue
+	if len(defaultValue) != 0 {
+		return defaultValue[0]
+	}
+	return ""
 }
