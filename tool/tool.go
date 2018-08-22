a8ebf50e3838f4b8c2cc86d687e456be755af9cc
elvizlai <sdrzlyz@gmail.com>
2018-08-22 10:08:39 +0800

    feat: MarshalToString can now with space indent

--- tool/tool.go
+++ tool/tool.go
@@ -1,6 +1,7 @@
 package tool
 
 import (
+	"bytes"
 	"encoding/json"
 
 	"github.com/golang/protobuf/jsonpb"
@@ -11,20 +12,44 @@ var jbm = &jsonpb.Marshaler{
 	EmitDefaults: true,
 }
 
+var jbmIndent = &jsonpb.Marshaler{
+	EmitDefaults: true,
+	Indent:       "  ",
+}
+
 // MarshalToString convert proto or struct to json string
-func MarshalToString(v interface{}) string {
+func MarshalToString(v interface{}, withIndent ...bool) string {
 	switch t := v.(type) {
 	case proto.Message:
-		s, err := jbm.MarshalToString(t)
+		var marshaler *jsonpb.Marshaler
+		if len(withIndent) != 0 && withIndent[0] {
+			marshaler = jbmIndent
+		} else {
+			marshaler = jbm
+		}
+		s, err := marshaler.MarshalToString(t)
 		if err != nil {
 			return err.Error()
 		}
 		return s
 	default:
-		data, err := json.Marshal(v)
+		var data []byte
+		var err error
+		if len(withIndent) != 0 && withIndent[0] {
+			data, err = json.MarshalIndent(v, "", "  ")
+		} else {
+			data, err = json.Marshal(v)
+		}
 		if err != nil {
 			return err.Error()
 		}
 		return string(data)
 	}
+}
+
+// MarshalProto convert proto to bytes
+func MarshalProto(pb proto.Message) []byte {
+	buff := &bytes.Buffer{}
+	jbm.Marshal(buff, pb)
+	return buff.Bytes()
 }
