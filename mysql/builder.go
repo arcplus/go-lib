d439fb6a99885895c2e03776b976a828a1fc862e
elvizlai <sdrzlyz@gmail.com>
2018-11-22 11:25:38 +0800

    sql builder insert

--- mysql/builder.go
+++ mysql/builder.go
@@ -27,8 +27,8 @@ import (
 
 type sBuilder struct {
 	smt   []byte
-	table []byte
 	expr  []byte
+	table []byte
 	cond  []byte
 	group []byte
 	order []byte
@@ -141,3 +141,136 @@ func (b *sBuilder) Build(args ...interface{}) (string, []interface{}, error) {
 
 	return buf.String(), nil, nil
 }
+
+// INSERT [LOW_PRIORITY | DELAYED | HIGH_PRIORITY] [IGNORE]
+//     [INTO] tbl_name
+//     insert_values
+//     [ON DUPLICATE KEY UPDATE assignment_list]
+
+// insert_values:
+//     [(col_name [, col_name] ...)]
+//     {VALUES | VALUE} (expr_list) [, (expr_list)] ...
+// |   SET assignment_list
+// |   [(col_name [, col_name] ...)]
+//     SELECT ...
+
+// expr_list:
+//     expr [, expr] ...
+
+// assignment:
+//     col_name = expr
+
+// assignment_list:
+//     assignment [, assignment] ...
+
+type iBuilder struct {
+	smt   []byte
+	pri   []byte
+	ig    []byte
+	table []byte
+	cls   []string
+	vls   []string
+	dup   []byte
+	args  []interface{}
+}
+
+func INSERT(table string) *iBuilder {
+	return &iBuilder{
+		smt:   []byte("INSERT"),
+		table: wrap("INTO " + table),
+	}
+}
+
+func (b *iBuilder) HIGHPRIORITY() *iBuilder {
+	b.pri = wrap("HIGH_PRIORITY")
+	return b
+}
+
+func (b *iBuilder) LOWPRIORITY() *iBuilder {
+	b.pri = wrap("LOW_PRIORITY")
+	return b
+}
+
+func (b *iBuilder) IGNORE() *iBuilder {
+	b.ig = wrap("IGNORE")
+	return b
+}
+
+func (b *iBuilder) COLUMN(cls []string) *iBuilder {
+	b.cls = cls
+	return b
+}
+
+func (b *iBuilder) VALUES(vls []string) *iBuilder {
+	b.vls = vls
+	return b
+}
+
+func (b *iBuilder) COLUMNVALUES(cv map[string]interface{}) *iBuilder {
+	l := len(cv)
+	b.cls = make([]string, l)
+	b.args = make([]interface{}, l)
+
+	i := 0
+	for k, v := range cv {
+		b.cls[i] = k
+		b.args[i] = v
+		i++
+	}
+
+	return b
+}
+
+func (b *iBuilder) ONDUPLICATE(cmd string) *iBuilder {
+	b.dup = wrap("ON DUPLICATE " + cmd)
+	return b
+}
+
+func (b *iBuilder) Build(args ...interface{}) (string, []interface{}, error) {
+	buf := &bytes.Buffer{}
+
+	buf.Write(b.smt)
+	buf.Write(b.pri)
+	buf.Write(b.ig)
+	buf.Write(b.table)
+	buf.Write(wrap("(" + strings.Join(b.cls, ", ") + ")"))
+
+	if b.vls == nil {
+		l := len(b.cls)
+		b.vls = make([]string, l)
+		for i := 0; i < l; i++ {
+			b.vls[i] = "?"
+		}
+	}
+
+	buf.Write(wrap("VALUES (" + strings.Join(b.vls, ", ") + ")"))
+	buf.Write(b.dup)
+	buf.Write(end)
+
+	if b.args != nil {
+		return buf.String(), b.args, nil
+	}
+
+	if l := len(args); l != 0 {
+		query, args, err := sqlx.Named(buf.String(), args[0])
+		if l > 1 {
+			query, args, err = sqlx.In(query, args...)
+		}
+		return query, args, err
+	}
+
+	return buf.String(), nil, nil
+}
+
+// TODO
+// UPDATE [LOW_PRIORITY] [IGNORE] table_reference
+//     SET assignment_list
+//     [WHERE where_condition]
+//     [ORDER BY ...]
+//     [LIMIT row_count]
+
+// assignment:
+//     col_name = value
+
+// assignment_list:
+//     assignment [, assignment] ...
--- mysql/builder_test.go
+++ mysql/builder_test.go
@@ -25,7 +25,7 @@ func Benchmark_reIndent(b *testing.B) {
 	}
 }
 
-func TestFrom(t *testing.T) {
+func TestSELECT(t *testing.T) {
 	arg := map[string]interface{}{
 		"published": true,
 		"authors":   []int{8, 19, 32, 44},
@@ -33,4 +33,19 @@ func TestFrom(t *testing.T) {
 
 	query, args, err := SELECT("users").EXPR("*").WHERE("id!='' AND published=:published").GROUPBYHAVING("id HAVING id IN (:authors)").ORDERBY("id DESC").LIMIT(10, 10).FOR("UPDATE").Build(arg, true)
 	t.Log(query, args, err)
+}
+
+func TestINSERT(t *testing.T) {
+	arg := map[string]interface{}{
+		"hello": "nihao",
+	}
+
+	t.Log(INSERT("users").HIGHPRIORITY().IGNORE().COLUMNVALUES(map[string]interface{}{
+		"id":   "1",
+		"name": "elvizlai",
+		"age":  18,
+	}).ONDUPLICATE("DO NOTHING").Build(arg))
+
+	t.Log(INSERT("users").COLUMN([]string{"id", "name", "salt"}).Build())
+	t.Log(INSERT("users").COLUMN([]string{"id", "name", "salt"}).VALUES([]string{"?", "'?'", "(SELECT ':hello')"}).Build(arg))
 }
