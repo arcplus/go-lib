9338aa288bde73c6318e4b5afc41170cf0c70c36
elvizlai <sdrzlyz@gmail.com>
2018-11-26 17:08:22 +0800

    feat: pb pkg

---
+++ pb/proto.go
@@ -0,0 +1,86 @@
+package pb
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"reflect"
+
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes"
+	"github.com/golang/protobuf/ptypes/any"
+)
+
+var (
+	pbm = &jsonpb.Marshaler{
+		EmitDefaults: true,
+		OrigName:     true,
+	}
+
+	pbu = &jsonpb.Unmarshaler{
+		AllowUnknownFields: true,
+	}
+)
+
+// Marshal marshals a protocol buffer into JSON.
+// caution: nil proto.Message returns null with no error
+func Marshal(pb proto.Message) ([]byte, error) {
+	buff := &bytes.Buffer{}
+	err := pbm.Marshal(buff, pb)
+	if err != nil && err.Error() == "Marshal called with nil" {
+		return []byte("null"), nil
+	}
+	return buff.Bytes(), err
+}
+
+// MustMarshal convert proto to JSON without error.
+func MustMarshal(pb proto.Message) []byte {
+	data, _ := Marshal(pb)
+	return data
+}
+
+// Unmarshal unmarshals a JSON object stream into a protocol buffer.
+// caution: should handle null manually
+func Unmarshal(in interface{}, pb proto.Message) error {
+	var reader io.Reader
+	switch t := in.(type) {
+	case []byte:
+		reader = bytes.NewBuffer(t)
+	case string:
+		reader = bytes.NewBufferString(t)
+	case io.Reader:
+		reader = t
+	default:
+		return errors.New("type not support")
+	}
+
+	return pbu.Unmarshal(reader, pb)
+}
+
+// MarshalAny takes the protocol buffer and encodes it into google.protobuf.Any.
+// it support []proto.Message to []*any.Any or proto.Message to [0]*any.Any
+func MarshalAny(p interface{}) []*any.Any {
+	// check if is proto
+	if v, ok := p.(proto.Message); ok {
+		a, _ := ptypes.MarshalAny(v)
+		return []*any.Any{a}
+	}
+
+	rv := reflect.ValueOf(p)
+
+	switch rv.Kind() {
+	case reflect.Slice:
+		l := rv.Len()
+		as := make([]*any.Any, l)
+		for i := 0; i < l; i++ {
+			pm, ok := rv.Index(i).Interface().(proto.Message)
+			if ok {
+				as[i], _ = ptypes.MarshalAny(pm)
+			}
+		}
+		return as
+	default:
+		return nil
+	}
+}
---
+++ pb/proto_test.go
@@ -0,0 +1,71 @@
+package pb
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/arcplus/go-lib/internal/pb"
+	"github.com/golang/protobuf/proto"
+)
+
+func TestMarshal(t *testing.T) {
+	var tp *pb.TestProto
+	data, err := Marshal(tp)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(string(data))
+
+	tp = &pb.TestProto{
+		Id:     "uuid",
+		Name:   "elvizlai",
+		Age:    18,
+		NextId: "next-uuid",
+		Filter: map[string]string{
+			"k1": "v1",
+		},
+	}
+	data, err = Marshal(tp)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(string(data))
+}
+
+const dataStr = `{"id":"uuid","name":"elvizlai","age":"18","nextId":"next-uuid","filter":{"k1":"v1"}}`
+
+func TestUnmarshal(t *testing.T) {
+	tp := &pb.TestProto{}
+
+	err := Unmarshal([]byte(dataStr), tp)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(tp)
+
+	buff := bytes.NewBuffer([]byte(dataStr))
+	err = Unmarshal(buff, tp)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(tp)
+
+	tp.Reset()
+
+	err = Unmarshal([]byte("null"), tp)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(tp == nil)
+}
+
+func TestMarshalAny(t *testing.T) {
+	list := make([]proto.Message, 3)
+	list[1] = &pb.TestProto{}
+
+	as := MarshalAny(list[1])
+	t.Log(as, len(as))
+
+	as = MarshalAny(list)
+	t.Log(as, len(as))
+}
