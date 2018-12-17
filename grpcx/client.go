12c2494ee6f3600ffb39775f50293c80f08bfb66
elvizlai <sdrzlyz@gmail.com>
2018-12-17 16:43:40 +0800

    feat: mv scaffold to grpcs

---
+++ grpcx/client.go
@@ -0,0 +1,25 @@
+package grpcx
+
+import (
+	"context"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/status"
+)
+
+func ClientErrorConvertor(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	err := invoker(ctx, method, req, reply, cc, opts...)
+	if err != nil {
+		// this must be gRPC error
+		s, ok := status.FromError(err)
+		if !ok {
+			return err
+		}
+
+		return &grpcErrorWrapper{
+			s: s,
+		}
+	}
+
+	return nil
+}
