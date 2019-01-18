2a53b66835d6fdf14cce9fa0c136397775a4d3ec
elvizlai <sdrzlyz@gmail.com>
2019-01-18 11:33:31 +0800

    feat: gen seq id by time for query

--- seq/seq.go
+++ seq/seq.go
@@ -7,25 +7,48 @@ import (
 	"github.com/sony/sonyflake"
 )
 
-var sf *sonyflake.Sonyflake
+var (
+	startTime    = time.Date(2016, 9, 1, 0, 0, 0, 0, time.UTC)
+	startTimeNum = toSonyflakeTime(startTime)
+	sf           *sonyflake.Sonyflake
+)
 
 func init() {
 	var st sonyflake.Settings
-	st.StartTime = time.Date(2016, 9, 1, 0, 0, 0, 0, time.UTC)
+	st.StartTime = startTime
 	sf = sonyflake.NewSonyflake(st)
 	if sf == nil {
 		panic("sonyflake not created")
 	}
 }
 
-// NextID return an unique id str
-func NextID() string {
-	id, _ := sf.NextID()
-	return strconv.FormatUint(id, 10)
-}
-
 // NextNumID returns number id
 func NextNumID() uint64 {
 	id, _ := sf.NextID()
 	return id
+}
+
+// NextID return an unique id str
+func NextID() string {
+	return strconv.FormatUint(NextNumID(), 10)
+}
+
+const sonyflakeTimeUnit = 1e7 // nsec, i.e. 10 msec
+
+func toSonyflakeTime(t time.Time) int64 {
+	return t.UTC().UnixNano() / sonyflakeTimeUnit
+}
+
+// NextNumIDByTime is used for gen seq for query [second accuracy]
+func NextNumIDByTime(t time.Time) uint64 {
+	elapsedTime := toSonyflakeTime(t) - startTimeNum
+	id := uint64(elapsedTime)<<(sonyflake.BitLenSequence+sonyflake.BitLenMachineID) |
+		uint64(0)<<sonyflake.BitLenMachineID |
+		uint64(0)
+	return id
+}
+
+// NextIDByTime is used for gen seq for query [second accuracy]
+func NextIDByTime(t time.Time) string {
+	return strconv.FormatUint(NextNumIDByTime(t), 10)
 }
--- seq/seq_test.go
+++ seq/seq_test.go
@@ -3,6 +3,9 @@ package seq
 import (
 	"sync"
 	"testing"
+	"time"
+
+	"github.com/sony/sonyflake"
 )
 
 func TestNextID(t *testing.T) {
@@ -29,4 +32,30 @@ func TestNextID(t *testing.T) {
 		idMap[id] = true
 	}
 	t.Log("ok")
+}
+
+func TestNextNumIDByTime(t *testing.T) {
+	seqStart := NextNumIDByTime(time.Now())
+
+	var seqList []uint64
+	n := 10000
+	for i := 0; i < n; i++ {
+		seqList = append(seqList, NextNumID())
+	}
+
+	t.Log(sonyflake.Decompose(seqList[n-1]))
+
+	st := int64(sonyflake.Decompose(seqList[n-1])["time"]) + startTimeNum
+	sec := st / 100
+	endTime := time.Unix(sec, 0).Add(time.Second)
+	seqEnd := NextNumIDByTime(endTime)
+
+	for i := range seqList {
+		if seqList[i] <= seqStart {
+			t.Fatal("all seq must gt seqStart")
+		}
+		if seqList[i] >= seqEnd {
+			t.Fatal("all seq must le seqEnd")
+		}
+	}
 }
